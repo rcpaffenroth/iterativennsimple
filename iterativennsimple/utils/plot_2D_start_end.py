5584dbeaa041f849795114d13fe09fbb10cab1c0
Randy Paffenroth <randy.paffenroth@gmail.com>
2023-11-21 11:04:51 +0000

    Made MNIST one-hot encoded and working on MLP

--- iterativennsimple/utils/plot_2D_start_end.py
+++ iterativennsimple/utils/plot_2D_start_end.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 def plot_2D_start_end(start, end, target, plot_points = 10):
     plt.figure()
 
-    plt.scatter(start[:plot_points,0], start[:plot_points,1])
-    plt.scatter(end[:plot_points,0], end[:plot_points,1])
-    dx = end[:plot_points,0]-start[:plot_points,0]
-    dy = end[:plot_points,1]-start[:plot_points,1]
-    plt.quiver(start[:plot_points,0], start[:plot_points,1], dx, dy, scale=1, scale_units='xy', angles='xy', color='r', width=0.005)
+    plt.scatter(start[:plot_points, 0], start[:plot_points, 1])
+    plt.scatter(end[:plot_points, 0], end[:plot_points, 1])
+    dx = end[:plot_points, 0]-start[:plot_points, 0]
+    dy = end[:plot_points, 1]-start[:plot_points, 1]
+    plt.quiver(start[:plot_points, 0], start[:plot_points, 1], dx, dy, scale=1, scale_units='xy', angles='xy', color='r', width=0.005)
 
-    plt.scatter(target[:,0], target[:,1], s=1)
+    plt.scatter(target[:, 0], target[:, 1], s=1)
     plt.gca().set_aspect('equal')
     plt.show()
