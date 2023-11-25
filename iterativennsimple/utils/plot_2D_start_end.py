1d9f3f63359ab324306ed0ab7a617c292044a216
Randy Paffenroth <randy.paffenroth@gmail.com>
2023-11-25 13:04:11 +0000

    Updated documentation and plotting

--- iterativennsimple/utils/plot_2D_start_end.py
+++ iterativennsimple/utils/plot_2D_start_end.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt     
 
-def plot_2D_start_end(start, end, target, plot_points = 10):
+def plot_2D_start_end(start_x, start_y, end_x, end_y, target_x, target_y, plot_points = 10):
     plt.figure()
 
-    plt.scatter(start[:plot_points, 0], start[:plot_points, 1])
-    plt.scatter(end[:plot_points, 0], end[:plot_points, 1])
-    dx = end[:plot_points, 0]-start[:plot_points, 0]
-    dy = end[:plot_points, 1]-start[:plot_points, 1]
-    plt.quiver(start[:plot_points, 0], start[:plot_points, 1], dx, dy, scale=1, scale_units='xy', angles='xy', color='r', width=0.005)
+    plt.scatter(start_x[:plot_points], start_y[:plot_points])
+    plt.scatter(end_x[:plot_points], end_y[:plot_points])
+    dx = end_x[:plot_points]-start_x[:plot_points]
+    dy = end_y[:plot_points]-start_y[:plot_points]
+    plt.quiver(start_x[:plot_points], start_y[:plot_points], dx, dy, scale=1, scale_units='xy', angles='xy', color='r', width=0.005)
 
-    plt.scatter(target[:, 0], target[:, 1], s=1)
+    plt.scatter(target_x[:], target_y[:], s=1)
     plt.gca().set_aspect('equal')
     plt.show()
