51a247773658869a9cf7a8d28402c47dff760ad9
Randy Paffenroth <randy.paffenroth@gmail.com>
2023-12-05 18:18:36 -0500

    Working!

--- scripts/install-torch-sparse.sh
+++ scripts/install-torch-sparse.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
 
-CUDA=cu117
-pip install torch-scatter torch-sparse -f https://data.pyg.org/whl/torch-2.0.1+${CUDA}.html
+rm -rf .venv
+python3 -m venv .venv
+source .venv/bin/activate
+CUDA=cu118
+
+pip install torch==2.1.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/${CUDA}
+pip install torch-scatter torch-sparse -f https://data.pyg.org/whl/torch-2.1.0+${CUDA}.html
+pip install pandas matplotlib pyarrow ipykernel ipympl jupyterlab
+pip install .
