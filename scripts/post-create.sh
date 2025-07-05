468ba8a8af5b88881c62185ff4c707482f071338
Randy Paffenroth <randy.paffenroth@gmail.com>
2025-07-05 17:12:14 -0400

    Better notebooks and README

--- scripts/post-create.sh
+++ scripts/post-create.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
 # Install the python dependencies
-poetry shell
 poetry install
-cd scripts; python3 generate_data.py
 
 # Or, install the developer version
 # poetry install --with dev
+
+# Or, install the developer version with the sparse dependencies
+# poetry install --with dev,sparse 
+
